e0b544f4d372837edd264c579e48fe35541549c5
B Burks <33607825+bburks@users.noreply.github.com>
2023-09-02 09:04:04 -0500

    cleaning

--- src/scripts/simulate_methylation.py
+++ src/scripts/simulate_methylation.py
@@ -1,10 +1,17 @@
-from src.tools import methylation
-import matplotlib.pyplot as plt
+"""
+This script runs a simulation, saves the results to a file (overwriting the past file),
+and plots the simulation.
+"""
 
+from src.tools import methylation
+from src.tools import plot
+from src.tools import io
+
+SIMULATION_PATH = "example"
 
 if __name__ == "__main__":
     site_count = 7
-    model = methylation.OneDimensionalMethylation(site_count)
+    model = methylation.OneDimensionalNonCollaborativeMethylation(site_count)
     parameters = {
         "r_um": 1,
         "r_mu": 1,
@@ -13,19 +20,10 @@ if __name__ == "__main__":
         "d_0": 0,
         "d_M": 0,
     }
-    timesteps = [i / 100 for i in range(1001)]
+    timesteps = [i / 1000 for i in range(1001)]
     initial_state = [100] * 8
-    result = model.generate_simulation_data(parameters, initial_state, timesteps)
+    #result = model.generate_simulation_data(parameters, initial_state, timesteps)
+    #io.write_simulation(result, SIMULATION_PATH)
+    result = io.read_simulation(SIMULATION_PATH)
+    plot.plot_timepoint_data(result)
     
-
-    fig, ax = plt.subplots()
-    xs =  list(result["data"].keys())
-    xs.sort()
-    for i in range(site_count + 1):
-        ys = []
-        for x in xs: 
-            ys.append(result["data"][x][i])
-        ax.plot(xs, ys, label=f"{i}")
-    plt.legend()
-    plt.show()
-    print(result)
