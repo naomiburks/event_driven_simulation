c4e3e37d08ebf3922726584c875249dd37aeb002
B Burks <33607825+bburks@users.noreply.github.com>
2024-03-27 11:00:36 -0500

    add folder select

--- main.py
+++ main.py
@@ -1,24 +1,29 @@
 from appJar import gui
 import os
 
-def press(name):
-    with open(f"src/scripts/{name}") as script:
-        exec(script.read())
+BASEPATH = "src/scripts/methylation"
 
 
-files = [filename for filename in os.listdir("src/scripts/") if filename[0] != "_"]
+def pick_script(path):
 
-app = gui()
-app.addOptionBox("Script", files)
+    def press(name):
+        next_path = f"{path}/{name}"
+        app.stop()
+        if os.path.isdir(next_path):
+            pick_script(next_path)
+        else:
+            with open(next_path) as script:
+                print(next_path)
+                exec(script.read())
 
-def run():
-    filename = app.getOptionBox("Script")
-    press(filename)
+    files = [filename for filename in os.listdir(path) if filename[0] != "_"]
+    app = gui()
+    app.addOptionBox("Script", files)
+    def run():
+        filename = app.getOptionBox("Script")
+        press(filename)
+    app.addButton("run", run)
+    app.go()
 
-app.addButton("run", run)
-#app.addLabel("title", "Pick a Script")
-#app.setLabelBg("title", "white")
-#for filename in files:
-#    app.addButton(filename, press)
+pick_script(BASEPATH)
 
-app.go()
