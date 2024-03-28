6c50073030eca62c141475485c53e79ef6a0a3d3
B Burks <33607825+bburks@users.noreply.github.com>
2024-03-28 10:45:33 -0500

    cleaning

--- src/tools/models/population.py
+++ src/tools/models/population.py
@@ -3,21 +3,22 @@ from scipy.linalg import expm, eig
 from src.tools.models.model import Model
 import numpy as np
 
+
 class PopulationModel(Model):
     """In a population model, the state space is a list of population counts
     for populations of various types."""
 
-    def __init__(self, population_count: int):
+    def __init__(self, num_of_populations: int):
         super().__init__()
-        self.population_count = population_count
+        self.num_of_populations = num_of_populations
 
     def sample_extinction(self, parameters, duration, num_attempts_per_pop):
         """Uses Monte Carlo to estimate extinction probabilities."""
         extinction_rates = []
-        for population_index in range(self.population_count):
+        for population_index in range(self.num_of_populations):
             num_extinctions = 0
             initial_state = self._standard_basis_vector(
-                population_index, self.population_count)
+                population_index, self.num_of_populations)
             for _ in range(num_attempts_per_pop):
                 final_state = self.run(parameters, initial_state, duration)
                 if sum(final_state) == 0:
@@ -35,19 +36,21 @@ class PopulationModel(Model):
 class ExponentialPopulationModel(PopulationModel):
     """
     This model is used to run deterministic model where populations grow in accordance
-    with the exponential of a generator matrix.
+    with the exponential of an instananeous generator matrix M.
+
+    The the entry M[i][j] is the rate at which a type-i individual generates type-j individuals.
     """
 
-    def __init__(self, population_count, generator_function):
+    def __init__(self, population_count, get_generator_from_parameters):
         super().__init__(population_count)
-        self.generator_function = generator_function
+        self.get_generator_from_parameters = get_generator_from_parameters
 
     def run(self, parameters, initial_state, duration):
         transition_matrix = self._get_instantaneous_transition_matrix(parameters)
         return list(initial_state @ expm(duration * transition_matrix))
 
     def get_long_term_behavior(self, parameters):
-        """Returns (growth rate, stable state)"""
+        """Returns a double (growth rate: float, stable population fractions: list)"""
         transition_matrix = self._get_instantaneous_transition_matrix(parameters)
         vals, vecs = eig(transition_matrix, left=True, right=False)
         max_val = None
@@ -61,20 +64,6 @@ class ExponentialPopulationModel(PopulationModel):
         return max_val, list(vec / sum(vec))
 
     def _get_instantaneous_transition_matrix(self, parameters):
-        return self.generator_function(parameters)
+        return self.get_generator_from_parameters(parameters)
 
-
-class ContinuumPopulationModel(Model):
-    """The state space of a continuum population model is a list of
-    individuals, where each individual has a type between 0 and 1.
-
-    Individuals have a deterministic drift, as well as stochastic birth and death rates. 
-    All are paremeter-dependent functions (type, params) -> num.
-    """
-
-    def __init__(self, drift, birthrate, deathrate):
-        super().__init__()
-        self.drift = drift
-        self.birthrate = birthrate
-        self.deathrate = deathrate
 
